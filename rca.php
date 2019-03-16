55ab31a66ba8f45a069d31a7d9b977d631b282d4
Timothy Cyrus <github@tcyr.us>
2019-03-16 15:21:44 -0400

    Updates

--- rca.php
+++ rca.php
@@ -6,4 +6,26 @@ use Grav\Common\Theme;
 class RcaTheme extends Theme
 {
 
+    public static function getSubscribedEvents()
+    {
+        return [
+            'onThemeInitialized'    => ['onThemeInitialized', 0],
+            'onTwigLoader'          => ['onTwigLoader', 0],
+        ];
+    }
+
+    public function onThemeInitialized()
+    {
+
+    }
+
+    // Add images to twig template paths to allow inclusion of SVG files
+    public function onTwigLoader()
+    {
+        $theme_paths = Grav::instance()['locator']->findResources('theme://images');
+        foreach($theme_paths as $images_path) {
+            $this->grav['twig']->addPath($images_path, 'images');
+        }
+    }
+
 }
